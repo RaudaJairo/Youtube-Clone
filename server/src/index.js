3fee5d26520e8a45ed3fbf7763c5ac38a2fd053a
Jairo Rauda Rosales <jairo.rauda@neoris.com>
2021-10-09 12:31:37 -0500

    Connect to DB

--- server/src/index.js
+++ server/src/index.js
@@ -1,6 +1,13 @@
 require('dotenv').config();
 const app = require('./app');
+const db = require('./database');
 
-app.listen(app.get('port'), () => {
-	console.log("Server on port:", app.get('port'))
+app.listen(app.get('port'), async () => {
+	console.log("Server on port:", app.get('port'));
+	const isConnected = await db.createConnection();
+	if(!isConnected){
+		console.error("Database is NOT connected");
+		process.exit(1);
+	}
+	console.log("Database is connected")
 })
