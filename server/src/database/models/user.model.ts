a192e083efec6b885df241190a654ea16020daeb
jairo_rauda <jairo.rauda@neoris.com>
2021-10-09 22:59:12 -0500

    Create user model

---
+++ server/src/database/models/user.model.ts
@@ -0,0 +1,35 @@
+import type {Sequelize} from "sequelize";
+import type UserModel from "../../types/database/models/userModel.types";
+import {DataTypes,UUIDV4} from "sequelize";
+
+const userModel: (sequelize: Sequelize) => UserModel = (sequelize) =>
+    sequelize.define("User", {
+        id: {
+            type: DataTypes.UUID,
+            defaultValue: UUIDV4,
+            primaryKey: true
+        },
+        username: {
+            type: DataTypes.STRING,
+            validate: {
+                notContains: {
+                    args: [" "],
+                    msg: "White spaces are not valid"
+                }
+            }
+        },
+        name: DataTypes.STRING,
+        email: {
+            type: DataTypes.STRING,
+            validate: {
+                isEmail: {msg: "Invalid Email"}
+            }
+        },
+        password: {
+            type: DataTypes.STRING
+        }
+    }, {
+        tableName: 'users'
+    });
+
+export default userModel;
